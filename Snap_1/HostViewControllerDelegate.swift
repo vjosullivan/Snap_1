a29ba984f0bfd319fe2901812894f75903add68f
Vince O'Sullivan (from home) <vjosullivan@gmail.com>
2017-06-07 14:46:15 +0100

    Add Host View Controller Delegate.

---
+++ Snap_1/HostViewControllerDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  HostViewControllerDelegate.swift
+//  Snap_1
+//
+//  Created by Vincent O'Sullivan on 07/06/2017.
+//  Copyright © 2017 Rose Cottage Industry. All rights reserved.
+//
+
+import Foundation
+
+protocol HostViewControllerDelegate {
+
+    func hostViewControllerDidCancel(controller: HostViewController)
+}
